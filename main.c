b940568cd64dc3d3c919ad7a2f9d85f9463b8615
Pontus Lundin <lundin.codeitez@gmail.com>
2019-12-03 08:02:30 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+
+void nextChar(char ch)
+{
+   char new;
+   {
+      char new = ch + 1;
+   }
+
+   return new;
+}
+
+float main(int ch)
+{
+   char *x;
+
+   x = 'a';
+
+   while (x++ =! 'z');
+      printf("%2c",x)
+      x == nextChar(x);
+
+   pritnf("%c\n",x);
+
+   return;
